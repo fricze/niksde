9c7a73d49ca497ce449ed1f6a9574e259768b51b
fricze <andrzej.fricze@gmail.com>
2019-10-27 17:01:06 +0100

    Loving those breaks

--- Circle2.js
+++ Circle2.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, PanResponder, Animated } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View, StyleSheet, PanResponder } from "react-native";
 
 const CIRCLE_RADIUS = 5;
 
@@ -19,62 +19,115 @@ const styles = StyleSheet.create({
     height: CIRCLE_RADIUS * 2,
     borderRadius: CIRCLE_RADIUS,
   },
+
+  line: {
+    position: "absolute",
+    top: 0,
+
+    borderColor: "#DE76CE",
+    borderStyle: "solid",
+    borderWidth: 0.5,
+    width: 0,
+    zIndex: 5,
+
+    backgroundColor: "#fff",
+    height: "100%",
+  },
 });
 
+const breaks = [100, 200, 300];
+
 const DefView = () => {
-  const [left, setLeft] = useState(0);
-  const [top, setTop] = useState(0);
+  const [position, setPosition] = useState({
+    top: 30,
+    left: 30,
+    right: 80,
+    bottom: 80,
+  });
 
-  const [right, setRight] = useState(50);
-  const [bottom, setBottom] = useState(50);
+  const [showBreak, setShowBreak] = useState(false);
 
-  const width = right - left;
-  const height = bottom - top;
-
-  const [wH, setWh] = useState({ w: width, h: height });
+  const width = position.right - position.left;
+  const height = position.bottom - position.top;
 
   const panResponderCorner = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (e, g) => {
-      setRight(Math.max(g.moveX, 0));
-      setBottom(Math.max(g.moveY, 0));
-    },
-    onPanResponderRelease: () => {
-      setWh({ w: width, h: height });
+      setPosition(s => ({
+        ...s,
+        right: parseInt(g.moveX),
+        bottom: parseInt(g.moveY),
+      }));
     },
   });
 
   const panResponderUpLeft = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (e, g) => {
-      setLeft(Math.max(g.moveX, 0));
-      setTop(Math.max(g.moveY, 0));
-    },
-    onPanResponderRelease: () => {
-      setWh({ w: width, h: height });
+      setPosition(s => ({
+        ...s,
+        left: parseInt(g.moveX),
+        top: parseInt(g.moveY),
+      }));
     },
   });
 
   const panResponderCenter = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (e, g) => {
-      setLeft(Math.max(g.moveX - wH.w / 2, 0));
-      setRight(Math.max(g.moveX + wH.w / 2, 0));
+      const left = g.moveX;
+      const top = g.moveY;
 
-      setTop(Math.max(g.moveY - wH.h / 2, 0));
-      setBottom(Math.max(g.moveY + wH.h / 2, 0));
+      const onBreak = breaks.find(breakVal => Math.abs(left - breakVal) < 15);
+
+      setShowBreak(onBreak);
+
+      if (onBreak) {
+        const left = onBreak;
+
+        setPosition(() => ({
+          left: parseInt(left - width / 2),
+          right: parseInt(left + width / 2),
+          top: parseInt(top - height / 2),
+          bottom: parseInt(top + height / 2),
+        }));
+      } else {
+        setPosition(() => ({
+          left: parseInt(left - width / 2),
+          right: parseInt(left + width / 2),
+          top: parseInt(top - height / 2),
+          bottom: parseInt(top + height / 2),
+        }));
+      }
+    },
+    onPanResponderRelease: () => {
+      setShowBreak(false);
     },
   });
 
   return (
     <>
+      {breaks.map(breakVal => (
+        <View
+          key={breakVal}
+          style={[
+            styles.line,
+            {
+              left: breakVal,
+              display: breakVal === showBreak ? "flex" : "none",
+            },
+          ]}
+        />
+      ))}
+
       <View
+        /* {...panResponderCenter.panHandlers} */
         style={[
           styles.box,
           {
             position: "absolute",
-            left,
-            top,
+            left: position.left,
+            top: position.top,
             width,
             height,
           },
