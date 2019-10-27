5cb6a6d3ad01ecf6f668fc16911b8ec729ff4251
fricze <andrzej.fricze@gmail.com>
2019-10-27 22:03:36 +0100

    Many elements

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-// @flow
-import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
-import CustomShape from "./Shape";
-import Draggable from "./Circle2";
+import React, { useState } from "react";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import Elements from "./Elements";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <CustomShape /> */}
-      <Draggable />
-      {/* <ScrollView> */}
-      {/* </ScrollView> */}
-    </SafeAreaView>
-  );
-}
+const breaks = [100, 200, 300];
+
+const elements = ["a", "b"];
 
 const styles = StyleSheet.create({
   container: {
@@ -23,4 +14,53 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "red",
   },
+
+  line: {
+    position: "absolute",
+    top: 0,
+
+    borderColor: "#DE76CE",
+    borderStyle: "solid",
+    borderWidth: 0.5,
+    width: 0,
+    zIndex: 5,
+
+    backgroundColor: "#fff",
+    height: "100%",
+  },
 });
+
+export default function App() {
+  const [showBreak, setShowBreak] = useState(false);
+
+  const [active, setActive] = useState(elements[0]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {elements.map(id => (
+        <Elements
+          key={id}
+          id={id}
+          active={active === id}
+          setActive={() => setActive(id)}
+          breaks={breaks}
+          showBreak={showBreak}
+          setShowBreak={setShowBreak}
+        />
+      ))}
+
+      {breaks.map(breakVal => (
+        <View
+          key={breakVal}
+          style={[
+            styles.line,
+            {
+              left: breakVal,
+              display: breakVal === showBreak ? "flex" : "none",
+            },
+          ]}
+        />
+      ))}
+    </SafeAreaView>
+  );
+}
--- Elements.js
+++ Elements.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { Text, TextInput, View, StyleSheet, PanResponder } from "react-native";
+import { View, StyleSheet, PanResponder } from "react-native";
 
 const CIRCLE_RADIUS = 5;
 
 const styles = StyleSheet.create({
   box: {
+    position: "absolute",
     backgroundColor: "skyblue",
+    borderWidth: 0.5,
+    borderStyle: "solid",
   },
 
   circle: {
-    borderColor: "#333",
+    borderColor: "#477BE3",
     borderStyle: "solid",
     borderWidth: 1,
 
@@ -19,33 +22,15 @@ const styles = StyleSheet.create({
     height: CIRCLE_RADIUS * 2,
     borderRadius: CIRCLE_RADIUS,
   },
-
-  line: {
-    position: "absolute",
-    top: 0,
-
-    borderColor: "#DE76CE",
-    borderStyle: "solid",
-    borderWidth: 0.5,
-    width: 0,
-    zIndex: 5,
-
-    backgroundColor: "#fff",
-    height: "100%",
-  },
 });
 
-const breaks = [100, 200, 300];
-
-const DefView = () => {
+const DefView = ({ active, setActive, breaks, setShowBreak }) => {
   const [position, setPosition] = useState({
     top: 30,
     left: 30,
     right: 80,
     bottom: 80,
   });
-
-  const [showBreak, setShowBreak] = useState(false);
 
   const width = position.right - position.left;
   const height = position.bottom - position.top;
@@ -106,76 +91,72 @@ const DefView = () => {
   });
 
   return (
-    <>
-      {breaks.map(breakVal => (
-        <View
-          key={breakVal}
-          style={[
-            styles.line,
-            {
-              left: breakVal,
-              display: breakVal === showBreak ? "flex" : "none",
-            },
-          ]}
-        />
-      ))}
+    <View
+      onStartShouldSetResponder={setActive}
+      style={[
+        styles.box,
+        {
+          left: position.left,
+          top: position.top,
+          borderColor: active ? "#477BE3" : "transparent",
+          width,
+          height,
+          zIndex: active ? 5 : 0,
+          shadowColor: active ? "#333" : "transparent",
+          shadowOffset: { width: 1, height: 1 },
+          shadowOpacity: 0.3,
+          shadowRadius: 1,
+        },
+      ]}
+    >
+      {active && (
+        <>
+          <View
+            {...panResponderUpLeft.panHandlers}
+            style={[
+              styles.circle,
+              {
+                position: "absolute",
+                left: "0%",
+                top: "0%",
+                marginLeft: -3,
+                marginTop: -3,
+              },
+            ]}
+          />
 
-      <View
-        /* {...panResponderCenter.panHandlers} */
-        style={[
-          styles.box,
-          {
-            position: "absolute",
-            left: position.left,
-            top: position.top,
-            width,
-            height,
-          },
-        ]}
-      >
-        <View
-          {...panResponderUpLeft.panHandlers}
-          style={[
-            styles.circle,
-            {
-              position: "absolute",
-              left: "0%",
-              top: "0%",
-              marginLeft: -3,
-              marginTop: -3,
-            },
-          ]}
-        />
+          <View
+            {...panResponderCorner.panHandlers}
+            style={[
+              styles.circle,
+              {
+                position: "absolute",
+                left: "100%",
+                top: "100%",
+                marginLeft: -6,
+                marginTop: -6,
+              },
+            ]}
+          />
 
-        <View
-          {...panResponderCorner.panHandlers}
-          style={[
-            styles.circle,
-            {
-              position: "absolute",
-              left: "100%",
-              top: "100%",
-              marginLeft: -6,
-              marginTop: -6,
-            },
-          ]}
-        />
-
-        <View
-          {...panResponderCenter.panHandlers}
-          style={[
-            styles.circle,
-            {
-              position: "absolute",
-              left: "50%",
-              top: "50%",
-              marginLeft: -6,
-              marginTop: -6,
-            },
-          ]}
-        />
-      </View>
-    </>
+          <View
+            {...panResponderCenter.panHandlers}
+            style={[
+              styles.circle,
+              {
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                marginLeft: -14,
+                marginTop: -14,
+                width: 30,
+                height: 30,
+              },
+            ]}
+          />
+        </>
+      )}
+    </View>
   );
 };
 
